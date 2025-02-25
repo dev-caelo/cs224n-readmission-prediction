ed0ff9f561ad0203c3d1a3ab4a994adb70da22e5
Carlo Dino <cdino@stanford.edu>
2025-02-24 19:46:53 -0800

    Forked over Hybrid_Fusion model arch, requirements, and setup code

---
+++ config/config.py
@@ -0,0 +1,26 @@
+# Forked and edited from original authors: https://github.com/haidog-yaqub/Clinical_HybridFusion/blob/main/config/get_config.py
+
+from torch.nn import Tanh
+
+def get_config(language_model):
+    if language_model == 'bert':
+        model_name = "bert-base-uncased"
+        bert_features = 768,
+        activation_func = Tanh()
+    elif language_model == "clinical_longformer":
+        model_name = "yikuan8/Clinical-Longformer"
+        bert_features = 768,
+        activation_func = Tanh()
+    elif language_model == "roberta":
+        model_name = "roberta-base"
+        bert_features = 768,
+        activation_func = Tanh()
+    elif language_model == "clinical_bert":
+        model_name = "emilyalsentzer/Bio_ClinicalBERT"
+        bert_features = 768,
+        activation_func = Tanh()
+    else:
+        print('supported models: bert, clinical_longformer, roberta')
+        return 'error'
+
+    return model_name, bert_features, activation_func
